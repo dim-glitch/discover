78d7205003cde0a24e6e487650e3e44c4ae4c3e1
Your Name <you@example.com>
2020-06-26 10:53:31 +0300

    Добавил анимацию

--- js/script.js
+++ js/script.js
@@ -14,15 +14,15 @@ $(function () {
         arrows: false,
         dots: true,
 
-        responsive:[{
-            breakpoint:600,
-            settings:{
-                dots:false,
-                centerMode:true,
-                centerPadding:'50px'
+        responsive: [{
+            breakpoint: 600,
+            settings: {
+                dots: false,
+                centerMode: true,
+                centerPadding: '50px'
             }
         }]
     });
 
-
+    AOS.init();
 });
